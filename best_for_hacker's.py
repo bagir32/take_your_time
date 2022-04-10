1a1a600028ccf3f3568f25877d68fce110da34eb
0x2nac0nda <58049681+abdallaabdalrhman@users.noreply.github.com>
2022-04-10 04:28:37 +0200

    Create best_for_hacker's.py

---
+++ best_for_hacker's.py
@@ -0,0 +1,19 @@
+mport webbrowser
+import time
+import colorama
+from colorama import Fore
+
+print(Fore.RED + '0x2nac0nda')
+# @0x2nac0nda
+
+# Comman Web Site Visited From Hacker's
+# By default your name save on the browser
+# Anyone can edit
+
+webbrowser.open('https://www.gmail.com')
+time.sleep(10)
+webbrowser.open('https://www.twitter.com')
+time.sleep(10)
+webbrowser.open('https://www.hackerone.com')
+time.sleep(10)
+webbrowser.open('http://www.linkedin.com')
